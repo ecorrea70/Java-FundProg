a84d3dae32918a1e43596275d0e45bc024edb75d
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-04-02 18:47:07 -0300

    Initial commit

---
+++ dias_idade/dias.java
@@ -0,0 +1,20 @@
+
+/**
+ * Escreva uma descrição da classe dias aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+
+import java.util.Scanner;
+
+public class dias
+{
+    public static void main (String args[]){
+        Scanner entrada = new Scanner(System.in);
+        
+        Sysem.out.println("Digite sua data de nascimento
+        
+        
+    }
+}
