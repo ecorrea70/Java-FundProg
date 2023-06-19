676ec3dd4326852098b65d403151cdc13183a911
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-06-19 18:48:18 -0300

    inicio TF

--- trabalhofinal/Paciente.java
+++ trabalhofinal/Paciente.java
@@ -7,27 +7,14 @@
  */
 public class Paciente
 {
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String convenio;
+    private int diagnostico;
+    private int numConsultas;
+    
+    
 
-    /**
-     * Construtor para objetos da classe Paciente
-     */
-    public Paciente()
-    {
-        // inicializa variáveis de instância
-        x = 0;
-    }
-
-    /**
-     * Um exemplo de um método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   um exemplo de um parâmetro de método
-     * @return     a soma de x e y 
-     */
-    public int sampleMethod(int y)
-    {
-        // escreva seu código aqui
-        return x + y;
-    }
+    
 }
