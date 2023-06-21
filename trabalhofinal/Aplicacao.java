36e18b824fb9961ea68a7ad77c4a0c6a24dbe062
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-06-20 21:44:47 -0300

    adição de alguns métodos

--- trabalhofinal/Aplicacao.java
+++ trabalhofinal/Aplicacao.java
@@ -8,10 +8,29 @@
 public class Aplicacao
 {
     public static void main (String args[]){
+    System.out.println("\n");
+    CadastroPacientes cp = new CadastroPacientes (10);
+    
     Paciente p1 = new Paciente ("Cláudio", "12345678910", "(51)994204100", "Particular", 5, 2);
+    Paciente p2 = new Paciente ("Alan", "10987654321", "(11)997883197", "SUS", 5, 8);
+    Paciente p3 = new Paciente ("Breno", "90986254321", "(21)987353197", "SUS", 5, 1);
+    cp.adicionarPaciente(p1);
+    cp.adicionarPaciente(p2);
+    cp.adicionarPaciente(p3);
+    
+    cp.ordemAlfabetica();
+    cp.mostraPacientes();
     
     
     
      
+    }
+    
+    public static void menu(){
+        System.out.println("Selecione uma opção: \n");
+        System.out.println("1 - Sair\n");
+        System.out.println("2 - Listar todos os pacientes\n");
+        System.out.println("3 - Listar todos os pacientes em ordem alfabética\n");
+        System.out.println("4 - Média das consultas dos pacientes\n");
     }
 }
--- trabalhofinal/CadastroPacientes.java
+++ trabalhofinal/CadastroPacientes.java
@@ -5,8 +5,36 @@
  * @author (seu nome) 
  * @version (um número da versão ou uma data)
  */
+import java.util.Arrays;
 public class CadastroPacientes
 {
+    private Paciente [] vetor;
+    private int i;
     
+    public CadastroPacientes(int tamanho) {
+        vetor = new Paciente[tamanho];
+        i=0;
+    }
+    
+    public boolean adicionarPaciente (Paciente a){
+        if(i >= vetor.length){
+            return false;
+        } else {
+            vetor[i] = a;
+            i++;
+            return true;
+        } 
+    }
+    
+    public void mostraPacientes(){
+        for(int n=0; n<i; n++){
+            System.out.println("\nCadastro de pacientes na posição" + (n+1)+ "\n********* ** ***** ** *******");            
+            System.out.println(vetor[n].toString());
+        }        
+    }
+    
+    public void ordemAlfabetica(){
+         Arrays.sort(vetor, 0, i, (paciente1, paciente2) -> paciente1.getNome().compareTo(paciente2.getNome()));
+    }
     
 }
