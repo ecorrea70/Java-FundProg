b4fe0b142e8be81b4782514f6461031a4b7b5700
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-06-23 00:35:29 -0300

    projeto finalizado

--- trabalhofinal/Aplicacao.java
+++ trabalhofinal/Aplicacao.java
@@ -9,91 +9,133 @@ import java.util.Scanner;
 public class Aplicacao
 {
     public static void main (String args[]){
-    Scanner entrada = new Scanner(System.in);
-    int opcao = 100;
+        Scanner entrada = new Scanner(System.in);
+        int opcao = 100;
         
         
-    System.out.println("\f");
-    CadastroPacientes cp = new CadastroPacientes (10);
+        System.out.println("\f");
+        CadastroPacientes cp = new CadastroPacientes (10);
     
-    Paciente p1 = new Paciente ("Cláudio", "12345678910", "(51)994204100", "Particular", "Covid", 2);
-    Paciente p2 = new Paciente ("Alan", "10987654321", "(11)997883197", "SUS", "Dengue", 8);
-    Paciente p3 = new Paciente ("Breno", "90986254321", "(21)987353197", "SUS", "Influenza", 1);
-    cp.adicionarPaciente(p1);
-    cp.adicionarPaciente(p2);
-    cp.adicionarPaciente(p3);
+        Paciente p1 = new Paciente("Cláudio", "12345678910", "(51)994204100", "Particular", "Covid", 2);
+        Paciente p2 = new Paciente("Alan", "10987654321", "(11)997883197", "SUS", "Dengue", 8);
+        Paciente p3 = new Paciente("Breno", "90986254321", "(21)987353197", "Plano", "Influenza", 1);
+        Paciente p4 = new Paciente("Diana", "98765432101", "(51)9988776655", "Particular", "Covid", 3);
+        Paciente p5 = new Paciente("Pedro", "87654321098", "(11)987654321", "SUS", "Dengue", 5);
+        Paciente p6 = new Paciente("Maria", "76543210986", "(21)999888777", "Particular", "Influenza", 2);
+        Paciente p7 = new Paciente("Luiza", "65432109876", "(31)987776655", "Plano", "Meningite", 4);
+        Paciente p8 = new Paciente("Rafaela", "54321098765", "(41)988887777", "SUS", "Labirintite", 1);
+        Paciente p9 = new Paciente("Anderson", "43210987654", "(15)999988887", "Particular", "Colesterol", 3);
+        Paciente p10 = new Paciente("Juliana", "32109876543", "(48)997776655", "SUS", "Asma", 2);
+
+        cp.adicionarPaciente(p1);
+        cp.adicionarPaciente(p2);
+        cp.adicionarPaciente(p3);
+        cp.adicionarPaciente(p4);
+        cp.adicionarPaciente(p5);
+        cp.adicionarPaciente(p6);
+        cp.adicionarPaciente(p7);
+        cp.adicionarPaciente(p8);
+        cp.adicionarPaciente(p9);
+        cp.adicionarPaciente(p10);
     
-    //cp.ordemAlfabetica();
-    //cp.mostraPacientes();
-    
-    while (opcao != 0) {
-    System.out.println("Digite uma opção: ");
-    menu();
-    opcao = Integer.parseInt(entrada.nextLine());
+        while (opcao != 0) {
+            System.out.println("\n\nDigite uma opção: ");
+            menu();
+            opcao = Integer.parseInt(entrada.nextLine());
 
-    switch (opcao) {
-        case 0:
-            break;
+            switch (opcao) {
+                case 0:
+                break;
         
-        case 1:
-            cp.mostraPacientes();
-            break;
+                case 1:
+                    cp.mostraPacientes();
+                    break;
 
-        case 2:
-            cp.ordemAlfabetica();
-            cp.mostraPacientes();
-            break;
+                case 2:
+                    cp.ordemAlfabetica();
+                    cp.mostraPacientes();
+                    break;
 
-        case 3:
-            System.out.println("A média de consulta dos pacientes é de: " + cp.mediaConsultas() + " consultas por mês!");
-            break;
+                case 3:
+                    System.out.println("A média de consulta dos pacientes é de: " + cp.mediaConsultas() + " consultas por mês!");
+                    break;
 
-        case 4:
-            cp.mostraPacientes();
-            System.out.println("Digite a posição do paciente que você deseja alterar o telefone: ");
-            int index = Integer.parseInt(entrada.nextLine());
-            System.out.println("Digite o novo número: ");
-            String novoTelefone = entrada.nextLine();
-            cp.mudarTelefone(index, novoTelefone);
-            break;
+                case 4:
+                    cp.mostraPacientes();
+                    System.out.println("Digite a posição do paciente que você deseja alterar o telefone: ");
+                    int index = Integer.parseInt(entrada.nextLine());
+                    System.out.println("Digite o novo número: ");
+                    String novoTelefone = entrada.nextLine();
+                    cp.mudarTelefone(index, novoTelefone);
+                    break;
         
-        case 5:
-            cp.mostraPacientes();
-            System.out.println("Digite a posição do paciente que você deseja alterar o connvênio: ");
-            int index2 = Integer.parseInt(entrada.nextLine());
-            System.out.println("Digite o novo convênio: ");
-            String novoConvenio = entrada.nextLine();
-            cp.mudarConvenio(index2, novoConvenio);
-            break;
+                case 5:
+                    cp.mostraPacientes();
+                    System.out.println("Digite a posição do paciente que você deseja alterar o connvênio: ");
+                    int index2 = Integer.parseInt(entrada.nextLine());
+                    System.out.println("Digite o novo convênio: ");
+                    String novoConvenio = entrada.nextLine();
+                    cp.mudarConvenio(index2, novoConvenio);
+                    break;
             
-        case 6:
-            cp.mostraPacientes();
-            System.out.println("Digite a posição do paciente que você deseja alterar o diagnóstico: ");
-            int index3 = Integer.parseInt(entrada.nextLine());
-            System.out.println("Digite o novo diagnśotico: ");
-            String novoDiagnostico = entrada.nextLine();
-            cp.mudarDiagnostico(index3, novoDiagnostico);
-            break;
+                case 6:
+                    cp.mostraPacientes();
+                    System.out.println("Digite a posição do paciente que você deseja alterar o diagnóstico: ");
+                    int index3 = Integer.parseInt(entrada.nextLine());
+                    System.out.println("Digite o novo diagnśotico: ");
+                    String novoDiagnostico = entrada.nextLine();
+                    cp.mudarDiagnostico(index3, novoDiagnostico);
+                    break;
             
-        default:
-            System.out.println("Opção inválida!!!");
-            break;
-    }
-}
+                case 7:
+                    cp.mostraPacientes();
+                    System.out.println("Digite a posição do paciente que você deseja alterar o número de consultas no mês: ");
+                    int index4 = Integer.parseInt(entrada.nextLine());
+                    System.out.println("Digite o novo número de consultas: ");
+                    int novoNumconsultas = Integer.parseInt(entrada.nextLine());
+                    cp.mudarNumconsultas(index4, novoNumconsultas);
+                    break;
+            
+                case 8:
+                    cp.mostraPacientes();
+                    System.out.println("Digite a posição do paciente que você deseja ver o valor a ser pago no mês: ");
+                    int index5 = Integer.parseInt(entrada.nextLine());
+                    System.out.println("Digite o valor das consultas: ");
+                    int valorConsulta = Integer.parseInt(entrada.nextLine());
+                    System.out.println("O valor total a ser pago no mês é: R$"+cp.calcularValormensal(index5, valorConsulta));
+                    break; 
+           
+                case 9:
+                    cp.porcentagemDoencas();
+                    break;
+            
+                case 10:
+                    cp.porcentagemConvenios();
+                    break;
+            
+                default:
+                    System.out.println("Opção inválida!!!");
+                    break;
+            }
+        }
 
-    System.out.println("Encerrando a aplicação...");
+        System.out.println("Encerrando a aplicação...");
     
     
      
     }
     
-    public static void menu(){
-        System.out.println("0 - Sair\n");
-        System.out.println("1 - Listar todos os pacientes\n");
-        System.out.println("2 - Ordenar os pacientes em ordem alfabética\n");
-        System.out.println("3 - Média das consultas dos pacientes\n");
-        System.out.println("4 - Alterar o telefone de algum paciente\n");
-        System.out.println("5 - Alterar o convênio de algum paciente\n");
-        System.out.println("6 - Alterar o diagnóstico de algum paciente\n");
-    }
+        public static void menu(){
+            System.out.println("0 - Sair\n");
+            System.out.println("1 - Listar todos os pacientes\n");
+            System.out.println("2 - Ordenar os pacientes em ordem alfabética\n");
+            System.out.println("3 - Média das consultas dos pacientes\n");
+            System.out.println("4 - Alterar o telefone de algum paciente\n");
+            System.out.println("5 - Alterar o convênio de algum paciente\n");
+            System.out.println("6 - Alterar o diagnóstico de algum paciente\n");
+            System.out.println("7 - Alterar o número de consultas de um paciente no mês\n");
+            System.out.println("8 - Visualizar o valor mensal a ser pago por cada paciente\n");
+            System.out.println("9 - Mostrar a porcentagem de pacientes com cada diagnóstico\n");
+            System.out.println("10 - Mostrar a porcentagem de pacientes com cada convênio\n");
+        }
 }
--- trabalhofinal/CadastroPacientes.java
+++ trabalhofinal/CadastroPacientes.java
@@ -28,7 +28,7 @@ public class CadastroPacientes
     
     public void mostraPacientes(){
         for(int n=0; n<i; n++){
-            System.out.println("\nCadastro de pacientes na posição " + (n)+ "\n********* ** ***** ** *******");            
+            System.out.println("\n\n\nCadastro de pacientes na posição " + (n)+ "\n********* ** ***** ** *******");            
             System.out.println(vetor[n].toString());
         }        
     }
@@ -69,4 +69,90 @@ public class CadastroPacientes
             System.out.println("Índice inválido. Verifique o valor informado.");
         }
     }
+    
+    public void mudarNumconsultas(int index, int novoNumconsultas){
+        if (index >= 0 && index < vetor.length) {
+            vetor[index].setNumconsultas(novoNumconsultas);
+            System.out.println("Número de consultas do paciente no mês alterado com sucesso.");
+        } else {
+            System.out.println("Índice inválido. Verifique o valor informado.");
+        }
+    }
+    
+    public int calcularValormensal(int index, int valorConsulta){
+        if (index >= 0 && index < vetor.length) {
+            int valormensal = vetor[index].getNumconsultas()*valorConsulta;
+            return valormensal;   
+        } else {
+            System.out.println("Índice inválido. Verifique o valor informado.");
+            return 0;
+        }
+    }
+    
+    public void porcentagemDoencas(){
+        int tamanho = i; 
+        int covid = 0;
+        int dengue = 0;
+        int influenza = 0;
+        int meningite = 0;
+        int outro = 0;
+    
+        for (int j = 0; j < tamanho; j++) {
+            String diagnostico = vetor[j].getDiagnostico();
+        
+            if (diagnostico.equalsIgnoreCase("covid")) {
+                covid++;
+            } else if (diagnostico.equalsIgnoreCase("dengue")) {
+                dengue++;
+            } else if (diagnostico.equalsIgnoreCase("influenza")) {
+                influenza++;
+            } else if (diagnostico.equalsIgnoreCase("meningite")) {
+                meningite++;
+            } else {
+                outro++;
+            }
+        }
+    
+        double porcentagemCovid = (double) covid / tamanho * 100;
+        double porcentagemDengue = (double) dengue / tamanho * 100;
+        double porcentagemInfluenza = (double) influenza / tamanho * 100;
+        double porcentagemMeningite = (double) meningite / tamanho * 100;
+        double porcentagemOutro = (double) outro / tamanho * 100;
+    
+        System.out.println("\nCOVID: " + porcentagemCovid + "%");
+        System.out.println("\nDengue: " + porcentagemDengue + "%");
+        System.out.println("\nInfluenza: " + porcentagemInfluenza + "%");
+        System.out.println("\nMeningite: " + porcentagemMeningite + "%");
+        System.out.println("\nOutro diagnóstico: " + porcentagemOutro + "%");
+    }
+
+    public void porcentagemConvenios(){
+        int tamanho = i; 
+        int sus = 0;
+        int particular = 0;
+        int plano = 0;
+    
+        for (int j = 0; j < tamanho; j++) {
+            String convenio = vetor[j].getConvenio();
+        
+            if (convenio.equalsIgnoreCase("sus")) {
+                sus++;
+            }  
+            if (convenio.equalsIgnoreCase("particular")) {
+                particular++;
+            }
+            if (convenio.equalsIgnoreCase("plano")) {
+                plano++;
+            }
+        }
+    
+        double porcentagemSUS = (double) sus / tamanho * 100;
+        double porcentagemParticular = (double) particular / tamanho * 100;
+        double porcentagemPlano = (double) plano / tamanho * 100;
+    
+        System.out.println("\nSUS: " + porcentagemSUS + "%");
+        System.out.println("\nParticular: " + porcentagemParticular + "%");
+        System.out.println("\nPlano: " + porcentagemPlano + "%");
+    }    
+
 }
